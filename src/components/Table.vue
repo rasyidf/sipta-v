e6f5677cefd2975f564e2e311bfd2a387ded5e5a
rasyidf <ufarasfa@gmail.com>
2021-02-25 16:03:00 +0700

    Update Pagination

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -100,65 +100,20 @@
       <p class="m-0 text-muted">
         Showing <span>{{pageStart}}</span> to <span>{{pageEnd}}</span> of <span>{{MaxItem}}</span> entries
       </p>
-      <ul class="pagination m-0 ms-auto">
-        <li class="page-item disabled">
-          <a class="page-link" href="#" tabindex="-1" aria-disabled="true">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <polyline points="15 6 9 12 15 18"></polyline>
-            </svg>
-            prev
-          </a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item active"><a class="page-link" href="#">2</a></li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item"><a class="page-link" href="#">4</a></li>
-        <li class="page-item"><a class="page-link" href="#">5</a></li>
-        <li class="page-item">
-          <a class="page-link" href="#">
-            next
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="icon"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-            >
-              <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-              <polyline points="9 6 15 12 9 18"></polyline>
-            </svg>
-          </a>
-        </li>
-      </ul>
+      <Pagination/>
     </div>
   </div>
 </template>
 
 <script>
+import Pagination from "@/components/Pagination.vue";
 export default {
   name: 'Table',
   props: {
     title: String,
     maxItems: Number, 
 
-  }
+  }, components: { Pagination }
 
 };
 </script>
