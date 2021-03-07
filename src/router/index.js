f373387c34a48659da1111b7c463ef1fcfe2f6a9
rasyidf <ufarasfa@gmail.com>
2021-03-07 10:59:20 +0700

    Update Router

--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ const routes = [
     path: '/docs',
     name: 'Docs',
     default: 'intro',
-    component: () => import(/* webpackChunkName: "docs" */ '../views/Docs.vue'),
+    component: () => import(/* webpackChunkName: "docs" */ '../views/docs/index.vue'),
     children: [
       {
         path: 'intro',
@@ -22,6 +22,7 @@ const routes = [
         path: 'changes',
         component: () => import(/* webpackChunkName: "changes" */ '../views/docs/changes.vue')
       },
+      /* Start */
       {
         path: 'init',
         component: () => import(/* webpackChunkName: "init" */ '../views/docs/start/init.vue')
@@ -29,10 +30,57 @@ const routes = [
       {
         path: 'preparation',
         component: () => import(/* webpackChunkName: "prep" */ '../views/docs/start/prep.vue')
-      }      ,
+      },
       {
         path: 'models',
         component: () => import(/* webpackChunkName: "model" */ '../views/docs/start/model.vue')
+      },
+      /* Data */
+      {
+        path: 'data',
+        component: () => import(/* webpackChunkName: "data" */ '../views/docs/data/data.vue')
+      }, 
+      {
+        path: 'classes',
+        component: () => import(/* webpackChunkName: "classes" */ '../views/docs/data/classes.vue')
+      },
+      {
+        path: 'features',
+        component: () => import(/* webpackChunkName: "features" */ '../views/docs/data/features.vue')
+      },
+      /* Models */
+      {
+        path: 'training',
+        component: () => import(/* webpackChunkName: "train" */ '../views/docs/model/train.vue')
+      }, 
+      {
+        path: 'logs',
+        component: () => import(/* webpackChunkName: "logs" */ '../views/docs/model/logs.vue')
+      },
+      {
+        path: 'reports',
+        component: () => import(/* webpackChunkName: "reports" */ '../views/docs/model/reports.vue')
+      },
+      {
+        path: 'interactions',
+        component: () => import(/* webpackChunkName: "interactions" */ '../views/docs/model/interactions.vue')
+      },
+      {
+        path: 'charts',
+        component: () => import(/* webpackChunkName: "charts" */ '../views/docs/model/charts.vue')
+      },
+      /* Webhooks*/
+      {
+        path: 'webhook-triggers',
+        component: () => import(/* webpackChunkName: "triggers" */ '../views/docs/webhook/triggers.vue')
+      },
+      {
+        path: 'webhook-flags',
+        component: () => import(/* webpackChunkName: "flags" */ '../views/docs/webhook/flags.vue')
+      },
+      {
+        path: 'webhook-examples',
+        component: () => import(/* webpackChunkName: "examples" */ '../views/docs/webhook/examples.vue')
       }
     ]
   },
